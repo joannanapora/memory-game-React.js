bb180262ccef5d2466f839c2ad3339d67fdfc352
Joanna Napora <joannanapora@Joannas-MacBook-Air.local>
2021-03-31 17:48:17 +0100

    feat: create assets file with icons for cards

--- src/card/card.component.tsx
+++ src/card/card.component.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./card.component.scss";
+import {ReactComponent as MemoryIcon} from '../assets/001-arc de triomphe.svg';
 
 const Card = () => {
   const [isFlipped, setIsFlipped] = useState(false);
@@ -12,10 +13,11 @@ const Card = () => {
     <div onClick={toggleClass} className='card'>
       <div className={isFlipped ? "card-inner-flipped" : "card-inner"}>
         <div className="card-face card-face-front">
-          <h3>:)</h3>
+          <h3>Memory Card</h3>
         </div>
         <div className="card-face card-face-back">
           <div className="card-content">
+          <MemoryIcon className='icon' />
             <div className="card-header"></div>
             <div className="card-body"></div>
           </div>
